ab2851b391a42d841a9e0166e91c9baa1294be24
MFK <musabkaya2019@gmail.com>
2026-02-13 19:27:56 +0300

    Panel func of rich library

---
+++ Exercises/rich_lib.py
@@ -0,0 +1,6 @@
+from rich.console import Console
+from rich.panel import Panel
+
+console = Console()
+
+console.print(Panel("[bold cyan]Wassup?[/bold cyan]\n1. Im MFK!\n2. Im from Turkiye!\n3. I have an enthusiasm in programming and cyber security!"))
